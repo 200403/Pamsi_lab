7c97f8c254de4e6f859289ac37680116ca1a976a
mariod93 <200403@student.pwr.wroc.pl>
2014-04-23 22:49:38 +0200

    lab4-poprawiona wersja
    Poprawione sprawozdanie

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,42 @@
-#include "slownik.hh"
+#include "tester.hh"
 #include <iostream>
 #include <time.h>
 using namespace std;
 
-
+string wejscie;
+void zamienNazwy(int i){
+	string nazwa="wejscie", numer, rozszerzenie=".txt";
+	ostringstream ss;
+	ss << i;
+	numer=ss.str();
+	wejscie=nazwa+numer+rozszerzenie;
+}
 
 
 int main(){
-	Tablica_asocjacyjna<int> tab;
-	tab.dodaj("abc",1);
-	tab.dodaj("aba",2);
-	tab.dodaj("axc",6);
-	tab.dodaj("bbc",5);
-	tab.dodaj("aaa",2);
-	int i;
-	// test operatora indeksowania
-	i=tab["aaa"];
-	cout << "wartosc pod 'aaa': " << i << endl;
-	tab["aaa"]=10;
-	i=tab["aaa"];
-	cout << "wartosc pod 'aaa': " << i<< endl;
+	Tester test;
+	test.otworzPlik("merge.csv");
+	test.symulacja();
+	test.zamknijPlik();
 
+
+
+
+	/*int tab[7]={10,100,1000,10000,100000,1000000,10000000};
+	ofstream plik;
+	srand(time(NULL));
+	for(int i=5; i<7; i++){
+		zamienNazwy(i);
+		plik.open(wejscie.c_str(), ios::out);
+		plik << tab[i];
+		for(int j=0; j<tab[i]; j++ ){
+			plik << "\n";
+			plik << rand()%1000;
+		}
+		plik.close();
+
+	}*/
+	return 0;
 
 }
 
